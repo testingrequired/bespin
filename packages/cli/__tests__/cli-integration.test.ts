609b35513e9881d8c69c31132e6aef00bab92e15
Kylee Tilley <kyleetilley@gmail.com>
2020-08-28 08:29:47 -0500

    Remove unneeded test for cli

--- packages/cli/__tests__/cli-integration.test.ts
+++ packages/cli/__tests__/cli-integration.test.ts
@@ -2,7 +2,7 @@ const { system, filesystem } = require("gluegun");
 
 const src = filesystem.path(__dirname, "..");
 
-const cli = async cmd =>
+const cli = async (cmd) =>
   system.run("node " + filesystem.path(src, "bin", "cli") + ` ${cmd}`);
 
 test("outputs version", async () => {
@@ -13,17 +13,4 @@ test("outputs version", async () => {
 test("outputs help", async () => {
   const output = await cli("--help");
   expect(output).toContain("0.1.0");
-});
-
-test("generates file", async () => {
-  const output = await cli("generate foo");
-
-  expect(output).toContain("Generated file at models/foo-model.ts");
-  const foomodel = filesystem.read("models/foo-model.ts");
-
-  expect(foomodel).toContain(`module.exports = {`);
-  expect(foomodel).toContain(`name: 'foo'`);
-
-  // cleanup artifact
-  filesystem.remove("models");
 });
