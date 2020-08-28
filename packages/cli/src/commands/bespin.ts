de3fb70f4335089d1ebc424749dde353fabe4e97
Kylee Tilley <kyleetilley@gmail.com>
2020-08-28 12:55:29 -0500

    Add pass print to cli

--- packages/cli/src/commands/bespin.ts
+++ packages/cli/src/commands/bespin.ts
@@ -1,12 +1,15 @@
 import { GluegunCommand } from "gluegun";
+import { TestResultState } from "@testingrequired/bespin-core";
 
 const command: GluegunCommand = {
   name: "bespin",
-  run: async toolbox => {
+  run: async (toolbox) => {
     const { print } = toolbox;
 
     print.info("bespin!");
-  }
+
+    print.info(TestResultState.PASS);
+  },
 };
 
 module.exports = command;
