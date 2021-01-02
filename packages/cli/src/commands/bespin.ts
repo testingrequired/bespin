f24a7841f1ea43483a65765925d77f00277e0de0
Kylee Tilley <kyleetilley@gmail.com>
2021-01-02 16:05:02 -0600

    Create terminal reporter for cli

--- packages/cli/src/commands/bespin.ts
+++ packages/cli/src/commands/bespin.ts
@@ -1,5 +1,12 @@
 import { GluegunToolbox } from "gluegun";
-import { TestResultState, Runner, Config } from "@testingrequired/bespin-core";
+import {
+  TestResultState,
+  Runner,
+  Config,
+  Reporter,
+  TestInTestFile,
+  TestResult,
+} from "@testingrequired/bespin-core";
 
 export const name = "bespin";
 
@@ -20,30 +27,61 @@ export const run = async (toolbox: GluegunToolbox) => {
 
   const config = await Config.load(configFilePath);
 
+  config.reporters.push(new TerminalReporter(toolbox));
+
   const runner = new Runner();
 
   const results = await runner.run(config, configFilePath);
-
-  results.forEach(([testInTestFile, { state, time, message }]) => {
-    const formattedTime = `${time.toFixed(2)}ms`;
-    const printMessage = `${testInTestFile.testFilePath}:${testInTestFile.testName} ${state} (${formattedTime})`;
-
-    if (state === TestResultState.PASS) {
-      print.success(printMessage);
-    } else {
-      print.error(`${printMessage}\nMessage:\n${message}`);
-    }
-  });
 
   const passingRun = results
     .map(([_, result]) => result)
     .every(({ state }) => state === TestResultState.PASS);
 
-  if (passingRun) {
-    print.success("PASS");
-  } else {
-    print.error(`FAIL`);
-  }
-
   process.exit(passingRun ? 0 : 1);
 };
+
+class TerminalReporter extends Reporter {
+  constructor(private toolbox: GluegunToolbox) {
+    super();
+  }
+
+  // @ts-ignore: Unused argument/s
+  onTestStart(testInTestFile: TestInTestFile): void {
+    // @ts-ignore
+  }
+
+  // @ts-ignore: Unused argument/s
+  onTestEnd(testInTestFile: TestInTestFile, testResult: TestResult): void {
+    // @ts-ignore
+  }
+
+  // @ts-ignore: Unused argument/s
+  onRunStart(testsInTestFiles: TestInTestFile[]): void {
+    // @ts-ignore
+  }
+
+  onRunEnd(results: [TestInTestFile, TestResult][]): void {
+    const { print } = this.toolbox;
+
+    results.forEach(([testInTestFile, { state, time, message }]) => {
+      const formattedTime = `${time.toFixed(2)}ms`;
+      const printMessage = `${testInTestFile.testFilePath}:${testInTestFile.testName} ${state} (${formattedTime})`;
+
+      if (state === TestResultState.PASS) {
+        print.success(printMessage);
+      } else {
+        print.error(`${printMessage}\nMessage:\n${message}`);
+      }
+    });
+
+    const passingRun = results
+      .map(([_, result]) => result)
+      .every(({ state }) => state === TestResultState.PASS);
+
+    if (passingRun) {
+      print.success("PASS");
+    } else {
+      print.error(`FAIL`);
+    }
+  }
+}
