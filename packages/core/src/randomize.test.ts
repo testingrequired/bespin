6aae19132dd9235234fd301e213d4cd2bbf28b4a
Kylee Tilley <kyleetilley@gmail.com>
2021-02-04 23:58:05 -0600

    Add basic randomize test

---
+++ packages/core/src/randomize.test.ts
@@ -0,0 +1,9 @@
+import { randomizeArray } from './randomize';
+
+describe('randomize', () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  it('should have the same length randomized', () => {
+    expect(randomizeArray(arr).length).toBe(arr.length);
+  });
+});
