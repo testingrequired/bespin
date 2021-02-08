c70affb6202e037085cd9ccd13e8dc68408b6a6d
Kylee Tilley <kyleetilley@gmail.com>
2021-02-07 18:47:50 -0600

    WIP

---
+++ packages/cli2/src/index.ts
@@ -0,0 +1,26 @@
+import { Config, Runtime, TestResultState } from '@testingrequired/bespin-core';
+import { CLIReporter } from './CLIReporter';
+
+export const run_cli = async () => {
+  const config = await Config.load('./bespin.config.js');
+
+  config.reporters.push(new CLIReporter());
+
+  // if (toolbox.parameters.options?.testFileFilter) {
+  //   config.settings.testFileFilter = toolbox.parameters.options?.testFileFilter;
+  // }
+
+  // if (toolbox.parameters.options?.testNameFilter) {
+  //   config.settings.testNameFilter = toolbox.parameters.options?.testNameFilter;
+  // }
+
+  const runtime = new Runtime(config);
+
+  const results = await runtime.run();
+
+  const passingRun = results
+    .map(([_, result]) => result)
+    .every(({ state }) => state === TestResultState.PASS);
+
+  process.exit(passingRun ? 0 : 1);
+};
