6b3c21e8b169796cd6b5ac80046650c47a96f84f
Kylee Tilley <kyleetilley@gmail.com>
2021-02-13 04:33:29 -0600

    Add config info to html-reporter

--- packages/html-reporter/src/index.ts
+++ packages/html-reporter/src/index.ts
@@ -4,17 +4,25 @@ import {
   Reporter,
   TestInTestFile,
   TestResult,
+  ValidConfig,
 } from '@testingrequired/bespin-core';
 import pug from 'pug';
 
 export class HtmlReporter extends Reporter {
+  private config?: ValidConfig;
+
   constructor(private fileName: string = 'report.html') {
     super();
+  }
+
+  onRunStart(config: ValidConfig) {
+    this.config = config;
   }
 
   onRunEnd(results: Array<[TestInTestFile, TestResult]>) {
     const html = pug.renderFile(path.join(__dirname, './report.pug'), {
       results,
+      config: this.config,
     });
 
     fs.writeFileSync(path.join(process.cwd(), this.fileName), html);
