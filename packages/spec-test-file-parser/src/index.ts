0181ee5e9659b201eea392e1333141ad54c45ff1
Kylee Tilley <kyleetilley@gmail.com>
2021-02-14 05:51:25 -0600

    Refactor

--- packages/spec-test-file-parser/src/index.ts
+++ packages/spec-test-file-parser/src/index.ts
@@ -7,13 +7,13 @@ export class SpecTestFileParser extends TestFileParser {
   async getTests(path: string): Promise<Array<TestInTestFile>> {
     const spec = new Spec(global);
 
-    spec.load(() => {
-      delete require.cache[require.resolve(path)];
-      require(path);
-    });
+    delete require.cache[require.resolve(path)];
+    spec.load(() => require(path));
 
-    return spec
-      .getTests()
-      .map(([testName, testfn]) => new TestInTestFile(path, testName, testfn));
+    const tests = spec.getTests();
+
+    return tests.map(
+      ([testName, testfn]) => new TestInTestFile(path, testName, testfn)
+    );
   }
 }
